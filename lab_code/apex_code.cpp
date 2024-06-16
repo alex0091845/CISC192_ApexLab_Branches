be67a3eebc8c29ccdeb3b263280bd9b1c832e3a4
Alex Chow <45185445+alex0091845@users.noreply.github.com>
2024-06-16 15:54:43 +0800

    Update apex_code.cpp

--- lab_code/apex_code.cpp
+++ lab_code/apex_code.cpp
@@ -5,20 +5,23 @@
 
 using namespace std;
 
+// NOTE: There should be at least one switch statement in some part of your lab.
+
 void middle_word_check(stringstream& cin, stringstream& cout)
 {
-    // Place code for getting the street address
-    // Pretend this is your main function and you can use cin/cout
-    // as you would in main.
+    // Place code for taking in a word and printing if 
+    // it should come before or after the word "middle."
+    // You can use cin/cout as you would in main.
 
     // If you want to print to the console for debugging use std::cout.
     // Use cout for your final solution.
 }
 
 void animal_abcs(stringstream &cin, stringstream &cout) {
-    // Place code for outputting the total sales of a coffee item
-    // Pretend this is your main function and you can use cin/cout
-    // as you would in main.
+    // Place code for taking in one character, either uppercase
+    // or lowercase. If it's a character from A-F (a-f), you should
+    // print the corresponding animal. Otherwise, print "<character>
+    // is not supported" (see assignment writeup).
 
     // If you want to print to the console for debugging use std::cout.
     // Use cout for your final solution.
@@ -26,9 +29,9 @@ void animal_abcs(stringstream &cin, stringstream &cout) {
 
 void delivery_range_check(std::stringstream &cin, std::stringstream &cout)
 {
-    // Place code for reading the first name and country here
-    // Pretend this is your main function and you can use cin/cout
-    // as you would in main.
+    // Place code for reading in the inputs, processing them,
+    // and printing whether the Uber driver can make it with their
+    // vehicle or not.
 
     // If you want to print to the console for debugging use std::cout.
     // Use cout for your final solution.
